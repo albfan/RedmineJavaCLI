e0d14e5ea70de9f2de66163c905c48108b5f1fb4
a11n <andre.diermann@gmail.com>
2015-03-13 16:51:56 +0100

    Added test for HistoryCommand. Added expected output resource for HistoryCommand.

---
+++ src/test/java/de/ad/tools/redmine/cli/command/HistoryCommandTest.java
@@ -0,0 +1,91 @@
+package de.ad.tools.redmine.cli.command;
+
+import com.taskadapter.redmineapi.Include;
+import com.taskadapter.redmineapi.IssueManager;
+import com.taskadapter.redmineapi.RedmineManager;
+import com.taskadapter.redmineapi.bean.Issue;
+import com.taskadapter.redmineapi.bean.Tracker;
+import com.taskadapter.redmineapi.bean.User;
+import de.ad.tools.redmine.cli.Configuration;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Date;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static de.ad.tools.redmine.cli.test.TestHelper.resourceToByteArray;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class HistoryCommandTest {
+  private Configuration configuration;
+  private PrintStream out;
+  private ByteArrayOutputStream stream;
+  private RedmineManager redmineManager;
+
+  private IssueManager issueManager;
+
+  private HistoryCommand command;
+
+  @Rule
+  public ExpectedException exception = ExpectedException.none();
+
+  @Before
+  public void setUp() throws Exception {
+    configuration = mock(Configuration.class);
+    when(configuration.isConnected()).thenReturn(true);
+
+    stream = new ByteArrayOutputStream();
+    out = new PrintStream(stream);
+
+    redmineManager = mock(RedmineManager.class);
+    issueManager = mock(IssueManager.class);
+
+    when(redmineManager.getIssueManager()).thenReturn(issueManager);
+
+    command = new HistoryCommand(configuration, out, redmineManager);
+  }
+
+  @Test
+  public void testCommand() throws Exception {
+    String[] arguments = new String[] { "1" };
+
+    Issue issue = createMockIssue(1);
+    when(issueManager.getIssueById(1, Include.journals)).thenReturn(issue);
+
+    command.process(arguments);
+
+    //Maybe it's faster to use byte[]
+    //However, String provides a better comparison in case of differences
+    String actual = new String(stream.toByteArray());
+    String expected =
+        new String(resourceToByteArray("/HistoryCommandOutput.txt"));
+
+    assertThat(actual).isEqualTo(expected);
+  }
+
+  private Issue createMockIssue(int id) {
+    Tracker tracker = mock(Tracker.class);
+    when(tracker.getName()).thenReturn("Bug");
+
+    Date createdOn = new Date();
+    Date updatedOn = new Date();
+
+    User author = mock(User.class);
+    when(author.getFullName()).thenReturn("John Doe");
+
+    Issue issue = mock(Issue.class);
+    when(issue.getTracker()).thenReturn(tracker);
+    when(issue.getId()).thenReturn(id);
+    when(issue.getSubject()).thenReturn("Subject of #" + id);
+    when(issue.getCreatedOn()).thenReturn(createdOn);
+    when(issue.getUpdatedOn()).thenReturn(updatedOn);
+    when(issue.getAuthor()).thenReturn(author);
+    when(issue.getDescription()).thenReturn("Description of #" + id);
+
+    return issue;
+  }
+}
